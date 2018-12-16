d89cbc2e232b4fff145ba02cc049af1d0505db22
mummywho <wuguifeng0401@163.com>
2018-12-16 22:26:24 +0800

    add two models

--- apps/attendance/models.py
+++ apps/attendance/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class AttendanceInfo(models.Model):
+    attendancetime = models.DateTimeField(auto_now_add=True)
+    person = models.ForeignKey(User)  # unique=True, 这个字段在表中必须有唯一值.
+    image = models.ImageField( verbose_name="考勤图片")
+
+    def __str__(self):
+        return self.person.name
+
+    class Meta:
+        verbose_name = '考勤信息'
+        verbose_name_plural = verbose_name
+        ordering = ['id']
--- apps/facedata/models.py
+++ apps/facedata/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FaceData(models.Model):
+
+    face_id = models.CharField(max_length=30, unique=True, verbose_name="人脸样本ID")  # unique=True, 这个字段在表中必须有唯一值.
+    face_name = models.CharField(max_length=30, verbose_name="人脸拼音名称")
+    face_name = models.CharField(max_length=30, verbose_name="人脸中文名称")
+    face_image_url = models.URLField(max_length=50, null=True, blank=True, verbose_name="人脸样本图片Url地址")
+
+    def __str__(self):
+        return self.face_name
+
+    class Meta:
+        verbose_name = '人脸样本信息'
+        verbose_name_plural = verbose_name
+        ordering = ['id']
+
